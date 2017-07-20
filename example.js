87efc529e7742ef64aacf2f5e993e931837434a2
kangwrite <kangwrite@163.com>
2017-07-20 16:39:53 +0800

    Update example.js

--- example.js
+++ example.js
@@ -1,5 +1,6 @@
 $(function () {
     $("div[href]").click(function (event) {
+        //此处检查协议是否被注册
         window.protocolCheck($(this).attr("href"),
             function () {
                 alert("protocol not recognized");
